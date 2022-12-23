fd3b294b9cc6398cff0384ce897458101720f014
최민재 <cmjcmj1230@gmail.com>
2022-12-23 12:26:38 +0900

    ⚡️ :: Add basic colors

---
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/color/DodamColor.kt
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.smartschool.components.color
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 도담도담 컬러를 정의합니다.
+ */
+object DodamColor {
+
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+    val BackgroundColor = Color(0xFFF3F4F6)
+
+    val MainColor = Color(0xFF0067BC)
+
+    val SongColor = Color(0xFFF09771)
+    val ScheduleColor = Color(0xFFFF585A)
+    val LostFoundColor = Color(0xFF3BACB6)
+    val ItMapColor = Color(0xFF25316D)
+
+    val TransparentColor = Color(0x00000000)
+}
