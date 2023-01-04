340cfd4f436a06e18e5c03af1a730cded676cdd2
최민재 <cmjcmj1230@gmail.com>
2023-01-04 19:09:38 +0900

    ⚡️ :: Add Custom Clickable

---
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/modifier/dodamClickable.kt
@@ -0,0 +1,33 @@
+package kr.hs.dgsw.smartschool.components.modifier
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @param onClick 클릭 됐을 때, 람다식 실행(null이라면 click 활성화를 하지 않음).
+ * @param rippleColor ripple 색상을 지정
+ * @param rippleEnable ripple 활성화 여부
+ */
+fun Modifier.dodamClickable(
+    onClick: (() -> Unit)? = null,
+    rippleColor: Color = Color.Unspecified,
+    rippleEnable: Boolean = true,
+) = composed {
+    onClick?.let { onClick ->
+        clickable(
+            onClick = { onClick() },
+            indication = rememberRipple(
+                color = rippleColor
+            ).takeIf {
+                rippleEnable
+            },
+            interactionSource = remember { MutableInteractionSource() }
+        )
+    }
+    this
+}
