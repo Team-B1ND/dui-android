6263e11e7378b2620647b855ca13ec43167eb64b
최민재 <cmjcmj1230@gmail.com>
2023-01-17 18:48:41 +0900

    ⚡️ :: Add Custom drop down menu box

--- dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/basic/DropDownMenu.kt
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/basic/DropDownMenu.kt
@@ -1,6 +1,5 @@
 package kr.hs.dgsw.smartschool.components.component.basic
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Surface
@@ -19,14 +17,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
+import androidx.compose.ui.window.PopupProperties
 import kr.hs.dgsw.smartschool.components.component.input.Input
 import kr.hs.dgsw.smartschool.components.theme.Body3
+import kr.hs.dgsw.smartschool.components.theme.DodamColor
 import kr.hs.dgsw.smartschool.components.theme.IcLeftArrow
 
 @Composable
@@ -36,6 +39,8 @@ fun DropDownMenu() {
     var selectedItem by remember { mutableStateOf("") }
     var textFieldSize by remember { mutableStateOf(Size.Zero) }
 
+    val focusRequester = remember { FocusRequester() }
+
     Column(
         modifier = Modifier.padding(20.dp)
     ) {
@@ -44,15 +49,19 @@ fun DropDownMenu() {
             onValueChange = { selectedItem = it },
             hint = "Selected Item",
             modifier = Modifier
-                .fillMaxWidth()
                 .onGloballyPositioned { coordinates ->
                     textFieldSize = coordinates.size.toSize()
-                },
+                }
+                .focusRequester(focusRequester)
+            ,
             trailingIcon = {
                 IcLeftArrow(
                     modifier = Modifier
                         .rotate(if (expanded) 90f else 270f)
-                        .clickable { expanded = !expanded }
+                        .clickable {
+                            expanded = !expanded
+                            focusRequester.requestFocus()
+                        }
                         .size(15.dp),
                     contentDescription = null
                 )
@@ -66,14 +75,20 @@ fun DropDownMenu() {
                     with(LocalDensity.current) {
                         textFieldSize.width.toDp()
                     }
-                )
+                ),
+            properties = PopupProperties(focusable = true)
         ) {
-            list.forEach { label ->
-                DropdownMenuItem(onClick = {
-                    selectedItem = label
-                    expanded = false
-                }) {
-                    Body3(text = label)
+            list.forEachIndexed { index, label ->
+                Column {
+                    DropdownMenuItem(onClick = {
+                        selectedItem = label
+                        expanded = false
+                    }) {
+                        Body3(text = label, textColor = if (selectedItem == label) DodamColor.MainColor400 else DodamColor.Black)
+                    }
+
+                    if (index != list.size - 1)
+                        Divider(color = DodamColor.Gray50)
                 }
             }
         }
@@ -90,7 +105,6 @@ fun DropDownMenuPreview() {
             DropDownMenu()
             DropDownMenu()
         }
-
     }
 }
 
