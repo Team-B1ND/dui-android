78987c1585d484ced41fffaa71890ce7c8648522
최민재 <cmjcmj1230@gmail.com>
2023-01-23 17:00:09 +0900

    ⚡️ :: Add Tabs

--- dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/tab/Tab.kt
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/tab/Tab.kt
@@ -1,26 +1,31 @@
 package kr.hs.dgsw.smartschool.components.component.tab
 
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.hs.dgsw.smartschool.components.component.Surface
 import kr.hs.dgsw.smartschool.components.component.basic.Divider
 import kr.hs.dgsw.smartschool.components.foundation.Text
 import kr.hs.dgsw.smartschool.components.theme.DodamColor
 import kr.hs.dgsw.smartschool.components.theme.DodamTheme
-import kr.hs.dgsw.smartschool.components.theme.Label2
 import kr.hs.dgsw.smartschool.components.theme.LocalContentColor
 
 @Composable
@@ -29,11 +34,14 @@ fun RowScope.Tab(
     selected: Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    showLabel: Boolean = true,
+    icon: (@Composable () -> Unit)? = null,
+    selectedColor: Color = DodamTheme.color.MainColor400,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
 
     val contentColor = if (selected) {
-        DodamTheme.color.MainColor400
+        selectedColor
     } else {
         DodamTheme.color.Gray400
     }
@@ -47,7 +55,7 @@ fun RowScope.Tab(
     CompositionLocalProvider(
         LocalContentColor provides contentColor
     ) {
-        Box(
+        Column(
             modifier = modifier
                 .selectable(
                     selected = selected,
@@ -58,19 +66,106 @@ fun RowScope.Tab(
                     indication = null,
                 )
         ) {
-            Column(modifier = Modifier.width(IntrinsicSize.Min)) {
-                Text(
-                    modifier = Modifier.padding(vertical = 8.dp),
+            if (showLabel && (icon == null))
+                LabelOnly(
                     text = text,
-                    style = if (selected) selectedStyle else unSelectStyle,
+                    selected = selected,
+                    selectedStyle = selectedStyle,
+                    unSelectStyle = unSelectStyle
                 )
-                if (selected) {
-                    Divider(
-                        color = DodamColor.MainColor400,
-                        thickness = 2.dp,
-                    )
-                }
+            else if (!showLabel && (icon != null))
+                IconOnly(icon = icon, selected = selected)
+            else if (showLabel && icon != null)
+                IconLabel(
+                    icon = icon,
+                    text = text,
+                    selected = selected,
+                    selectedStyle = selectedStyle,
+                    unSelectStyle = unSelectStyle
+                )
+            if (selected) {
+                Divider(
+                    color = DodamColor.MainColor400,
+                    thickness = 2.dp,
+                )
             }
+        }
+    }
+}
+
+@Composable
+private fun LabelOnly(
+    text: String,
+    selected: Boolean,
+    selectedStyle: TextStyle,
+    unSelectStyle: TextStyle,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.CenterHorizontally),
+            text = text,
+            style = if (selected) selectedStyle else unSelectStyle,
+        )
+    }
+}
+
+@Composable
+private fun IconOnly(
+    icon: @Composable () -> Unit,
+    selected: Boolean,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(vertical = 13.dp)
+                .size(20.dp)
+                .align(Alignment.CenterHorizontally),
+        ) {
+            icon()
+        }
+    }
+}
+
+@Composable
+private fun IconLabel(
+    icon: @Composable () -> Unit,
+    text: String,
+    selected: Boolean,
+    selectedStyle: TextStyle,
+    unSelectStyle: TextStyle,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(7.dp)
+                .align(Alignment.CenterHorizontally),
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(20.dp)
+                    .align(Alignment.CenterHorizontally),
+            ) {
+                icon()
+            }
+            Spacer(modifier = Modifier.height(1.dp))
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                text = text,
+                style =
+                    if (selected)
+                        selectedStyle.copy(fontSize = 11.sp)
+                    else
+                        unSelectStyle.copy(fontSize = 11.sp),
+            )
         }
     }
 }
---
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/tab/Tabs.kt
@@ -0,0 +1,142 @@
+package kr.hs.dgsw.smartschool.components.component.tab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.hs.dgsw.smartschool.components.component.Surface
+import kr.hs.dgsw.smartschool.components.theme.DodamTheme
+import kr.hs.dgsw.smartschool.components.theme.IcHome
+import kr.hs.dgsw.smartschool.components.theme.IcSearch
+import kr.hs.dgsw.smartschool.components.theme.IcSong
+import kr.hs.dgsw.smartschool.components.theme.contentColorFor
+
+@Composable
+fun Tabs(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = DodamTheme.color.White,
+    contentColor: Color = contentColorFor(backgroundColor = backgroundColor),
+    content: @Composable RowScope.() -> Unit,
+) {
+    Surface(
+        color = backgroundColor,
+        contentColor = contentColor,
+        modifier = modifier,
+    ) {
+        Row(
+           modifier = Modifier
+               .selectableGroup()
+               .wrapContentWidth(Alignment.CenterHorizontally),
+           content = content,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TabsPreview() {
+    Column {
+        var selectedTab1 by remember { mutableStateOf(1) }
+        Tabs(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Tab(
+                text = "ITEM ONE",
+                selected = selectedTab1 == 1,
+                onClick = { selectedTab1 = 1 },
+                modifier = Modifier.weight(1f),
+            )
+            Tab(
+                text = "ITEM TWO",
+                selected = selectedTab1 == 2,
+                onClick = { selectedTab1 = 2 },
+                modifier = Modifier.weight(1f),
+            )
+            Tab(
+                text = "ITEM THREE",
+                selected = selectedTab1 == 3,
+                onClick = { selectedTab1 = 3 },
+                modifier = Modifier
+                    .weight(1f),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        var selectedTab2 by remember { mutableStateOf(1) }
+        Tabs(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Tab(
+                text = "ITEM ONE",
+                selected = selectedTab2 == 1,
+                onClick = { selectedTab2 = 1 },
+                modifier = Modifier.weight(1f),
+                icon = { IcHome(contentDescription = null) },
+            )
+            Tab(
+                text = "ITEM TWO",
+                selected = selectedTab2 == 2,
+                onClick = { selectedTab2 = 2 },
+                modifier = Modifier.weight(1f),
+                icon = { IcSong(contentDescription = null) },
+            )
+            Tab(
+                text = "ITEM THREE",
+                selected = selectedTab2 == 3,
+                onClick = { selectedTab2 = 3 },
+                modifier = Modifier
+                    .weight(1f),
+                icon = { IcSearch(contentDescription = null) },
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        var selectedTab3 by remember { mutableStateOf(1) }
+        Tabs(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Tab(
+                text = "ITEM ONE",
+                selected = selectedTab3 == 1,
+                onClick = { selectedTab3 = 1 },
+                modifier = Modifier.weight(1f),
+                icon = { IcHome(contentDescription = null) },
+                showLabel = false,
+            )
+            Tab(
+                text = "ITEM TWO",
+                selected = selectedTab3 == 2,
+                onClick = { selectedTab3 = 2 },
+                modifier = Modifier.weight(1f),
+                icon = { IcSong(contentDescription = null) },
+                showLabel = false,
+            )
+            Tab(
+                text = "ITEM THREE",
+                selected = selectedTab3 == 3,
+                onClick = { selectedTab3 = 3 },
+                modifier = Modifier
+                    .weight(1f),
+                icon = { IcSearch(contentDescription = null) },
+                showLabel = false,
+            )
+        }
+    }
+
+}
