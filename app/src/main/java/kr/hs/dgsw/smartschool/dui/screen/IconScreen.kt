62fc2964c426578c102f6ffdcd234c58948ae968
DoHyeon <kdh200533@gmail.com>
2023-01-28 20:49:03 +0900

    ⚡️ :: Add AppBar
    2023-01-28

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/IconScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/IconScreen.kt
@@ -1,29 +1,35 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import kr.hs.dgsw.smartschool.components.foundation.Text
+import kr.hs.dgsw.smartschool.dui.compose.ScreenAppBar
 
 
 @Composable
 @Preview(showBackground = true)
 fun IconPreview(){
+    IconScreen(viewModel = null, navController = rememberNavController())
 }
 
 @Composable
 fun IconScreen(
-    viewModel: ViewModel,
+    viewModel: ViewModel?,
     navController: NavController
 ){
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Text(text = "TypoScreen입니다.")
-    }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            ScreenAppBar(title = "IconScreen", navController = navController)
+            Text(text = "IconScreen입니다.")
+        }
 }
