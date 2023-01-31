023e52d602904de6e40135a844debe95e7bc3076
DoHyeon <kdh200533@gmail.com>
2023-01-31 19:03:29 +0900

    🎨 :: ktlint #2
    2023-01-31

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/ColorScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/ColorScreen.kt
@@ -1,5 +1,4 @@
 package kr.hs.dgsw.smartschool.dui.screen
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
