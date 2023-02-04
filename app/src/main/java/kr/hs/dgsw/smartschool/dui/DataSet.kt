c2660cbe2c0eccb403e942c39104f19561d6196e
DoHyeon <kdh200533@gmail.com>
2023-02-04 22:39:13 +0900

    ⚡️ :: Modify DataSet
    2023-02-04

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/DataSet.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/DataSet.kt
@@ -11,9 +11,9 @@ object DataSet {
         Item(kr.hs.dgsw.smartschool.dui.R.drawable.ic_palette_3d, "Color", "도담도담은 어떤 색을 사용할까?", "color"),
         Item(R.drawable.ic_info_3d, "Typo", "테스트를 위한 이런 데이터도 괜찮지 않을까요..?", "typo"),
         Item(R.drawable.ic_info_3d, "Icon", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "icon"),
-        Item(R.drawable.ic_info_3d, "Radius", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "radius"),
-        Item(R.drawable.ic_info_3d, "Bar", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "bar"),
-        Item(R.drawable.ic_info_3d, "Badge", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "badge"),
+        Item(R.drawable.ic_info_3d, "Shape", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "shape"),
+        Item(R.drawable.ic_info_3d, "Button", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "button"),
+        Item(R.drawable.ic_info_3d, "Input", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "Input"),
         Item(R.drawable.ic_info_3d, "Button", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "button"),
         Item(R.drawable.ic_info_3d, "Chip", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "chip"),
         Item(R.drawable.ic_info_3d, "ETC", "테스트를 위한 이런 데이터도 괜찮지 않을까요?", "etc")
