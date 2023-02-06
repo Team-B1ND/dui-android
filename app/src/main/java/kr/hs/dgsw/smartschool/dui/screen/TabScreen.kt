1e7be010d66eef177cb4eb4e0d48830aa310a8e1
DoHyeon <kdh200533@gmail.com>
2023-02-06 21:30:19 +0900

    ⚡️ :: Modify TabScreen
    2023-02-06

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/TabScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/TabScreen.kt
@@ -1,11 +1,7 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -17,9 +13,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import kr.hs.dgsw.smartschool.components.component.button.Button
+import kr.hs.dgsw.smartschool.components.component.button.DodamMediumRoundedButton
 import kr.hs.dgsw.smartschool.components.component.input.Input
 import kr.hs.dgsw.smartschool.components.component.tab.Tab
 import kr.hs.dgsw.smartschool.components.component.tab.Tabs
+import kr.hs.dgsw.smartschool.components.modifier.dodamClickable
+import kr.hs.dgsw.smartschool.components.theme.Body3
 import kr.hs.dgsw.smartschool.components.theme.Title2
 import kr.hs.dgsw.smartschool.components.theme.DodamColor
 import kr.hs.dgsw.smartschool.components.theme.IcHome
@@ -44,67 +44,97 @@ fun TabScreen(
     val selectedTab3 = remember {
         mutableStateOf(1)
     }
-    val itemCount = remember {
-        mutableStateOf(2)
-    }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(DodamColor.White)
+            .background(DodamColor.Background)
     ) {
         ScreenAppBar(title = "IconScreen", navController = navController)
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(20.dp)
-                .background(DodamColor.White)
+                .background(DodamColor.Background)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Spacer(modifier = Modifier.height(20.dp))
-            Input(
-                value = itemCount.value.toString(),
-                onValueChange = {itemCount.value = it.toInt()},
-                hint = "count"
-            )
+        ){
             Spacer(modifier = Modifier.height(20.dp))
             Title2(text = "Label Only")
             Spacer(modifier = Modifier.height(20.dp))
             Tabs() {
-                for(i : Int in 2..itemCount.value){
-                    Tab(
-                        text = "TEST",
-                        selected = selectedTab1.value == i,
-                        onClick = { selectedTab1.value = i }
-                    )
-                }
+                Tab(
+                    text = "Tab1",
+                    selected = selectedTab1.value == 1,
+                    onClick = { selectedTab1.value = 1 },
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab2",
+                    selected = selectedTab1.value == 2,
+                    onClick = { selectedTab1.value = 2 },
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab3",
+                    selected = selectedTab1.value == 3,
+                    onClick = { selectedTab1.value = 3 },
+                    modifier = Modifier.weight(1f),
+                )
             }
             Spacer(modifier = Modifier.height(20.dp))
             Title2(text = "Label O, Icon O")
             Spacer(modifier = Modifier.height(20.dp))
             Tabs() {
-                for(i : Int in 2..itemCount.value){
-                    Tab(
-                        text = "TEST",
-                        selected = selectedTab1.value == i,
-                        onClick = { selectedTab1.value = i },
-                        icon = { IcHome(contentDescription = null) },
-                    )
-                }
+                Tab(
+                    text = "Tab1",
+                    selected = selectedTab2.value == 1,
+                    onClick = { selectedTab2.value = 1 },
+                    icon = { IcHome(contentDescription = null)},
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab2",
+                    selected = selectedTab2.value == 2,
+                    onClick = { selectedTab2.value = 2 },
+                    icon = { IcHome(contentDescription = null)},
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab3",
+                    selected = selectedTab2.value == 3,
+                    onClick = { selectedTab2.value = 3 },
+                    icon = { IcHome(contentDescription = null)},
+                    modifier = Modifier.weight(1f),
+                )
             }
             Spacer(modifier = Modifier.height(20.dp))
             Title2(text = "Icon Only")
             Spacer(modifier = Modifier.height(20.dp))
             Tabs() {
-                for(i : Int in 2..itemCount.value){
-                    Tab(
-                        text = "TEST",
-                        selected = selectedTab1.value == i,
-                        onClick = { selectedTab1.value = i },
-                        icon = { IcHome(contentDescription = null) },
-                        showLabel = false
-                    )
-                }
+                Tab(
+                    text = "Tab1",
+                    selected = selectedTab3.value == 1,
+                    onClick = { selectedTab3.value = 1 },
+                    icon = { IcHome(contentDescription = null) },
+                    showLabel = false,
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab2",
+                    selected = selectedTab3.value == 2,
+                    onClick = { selectedTab3.value = 2 },
+                    icon = { IcHome(contentDescription = null) },
+                    showLabel = false,
+                    modifier = Modifier.weight(1f),
+                )
+                Tab(
+                    text = "Tab3",
+                    selected = selectedTab3.value == 3,
+                    onClick = { selectedTab3.value = 3 },
+                    icon = { IcHome(contentDescription = null) },
+                    showLabel = false,
+                    modifier = Modifier.weight(1f),
+                )
             }
             Spacer(modifier = Modifier.height(20.dp))
         }
