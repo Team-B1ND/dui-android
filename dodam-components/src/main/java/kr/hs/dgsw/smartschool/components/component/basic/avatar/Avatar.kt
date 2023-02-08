945cc8abc233c8c0b99d804927d1536a9fad9b57
최민재 <cmjcmj1230@gmail.com>
2023-02-08 18:32:33 +0900

    ⚡️ :: Add Link avatar and clickable
    + annotation

--- dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/basic/avatar/Avatar.kt
+++ dodam-components/src/main/java/kr/hs/dgsw/smartschool/components/component/basic/avatar/Avatar.kt
@@ -2,6 +2,8 @@ package kr.hs.dgsw.smartschool.components.component.basic.avatar
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,24 +18,46 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.components.rememberImageComponent
+import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
 import kr.hs.dgsw.smartschool.components.component.basic.surface
 import kr.hs.dgsw.smartschool.components.foundation.Text
+import kr.hs.dgsw.smartschool.components.modifier.dodamClickable
 import kr.hs.dgsw.smartschool.components.theme.DodamColor
 import kr.hs.dgsw.smartschool.components.theme.DodamTheme
 import kr.hs.dgsw.smartschool.components.theme.IcUser
 import kr.hs.smartschool.components.R
 
+/**
+ * Dodam Avatar, can express saved image
+ *
+ * @param painter profile image
+ * @param modifier
+ * @param shape shape of avatar, basic is circle
+ * @param size avatar circle size, basic is 40.dp
+ * @param rippleColor
+ * @param rippleEnable
+ * @param bounded
+ * @param onClick when click avatar
+ */
 @Composable
 fun Avatar(
     painter: Painter,
     modifier: Modifier = Modifier,
     shape: Shape = CircleShape,
     size: Dp = 40.dp,
+    rippleColor: Color = Color.Unspecified,
+    rippleEnable: Boolean = true,
+    bounded: Boolean = true,
+    onClick: (() -> Unit)? = null,
 ) {
     Box(
         modifier = modifier
@@ -42,13 +66,34 @@ fun Avatar(
         Image(
             modifier = Modifier
                 .clip(shape)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .dodamClickable(
+                    onClick = onClick,
+                    rippleColor = rippleColor,
+                    rippleEnable = rippleEnable,
+                    bounded = bounded
+                ),
             painter = painter,
             contentDescription = null
         )
     }
 }
 
+/**
+ * Dodam Avatar, can express first word of name
+ *
+ * @param name show first word
+ * @param modifier
+ * @param nameStyle text Style
+ * @param nameColor text Color
+ * @param backgroundColor color for circle background
+ * @param shape shape of avatar, basic is circle
+ * @param size avatar circle size, basic is 40.dp
+ * @param rippleColor
+ * @param rippleEnable
+ * @param bounded
+ * @param onClick when click avatar
+ */
 @Composable
 fun Avatar(
     name: String,
@@ -58,11 +103,21 @@ fun Avatar(
     backgroundColor: Color = DodamTheme.color.Background,
     shape: Shape = CircleShape,
     size: Dp = 40.dp,
+    rippleColor: Color = Color.Unspecified,
+    rippleEnable: Boolean = true,
+    bounded: Boolean = true,
+    onClick: (() -> Unit)? = null,
 ) {
     Box(
         modifier = modifier
             .size(size)
-            .surface(shape, backgroundColor),
+            .surface(shape, backgroundColor)
+            .dodamClickable(
+                onClick = onClick,
+                rippleColor = rippleColor,
+                rippleEnable = rippleEnable,
+                bounded = bounded
+            ),
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -73,6 +128,20 @@ fun Avatar(
     }
 }
 
+/**
+ * Dodam Avatar, can express icon center
+ *
+ * @param modifier
+ * @param iconSize icon's size
+ * @param iconColor icon's color
+ * @param backgroundColor color for circle background
+ * @param shape shape of avatar, basic is circle
+ * @param size avatar circle size, basic is 40.dp
+ * @param rippleColor
+ * @param rippleEnable
+ * @param bounded
+ * @param onClick when click avatar
+ */
 @Composable
 fun Avatar(
     modifier: Modifier = Modifier,
@@ -81,17 +150,99 @@ fun Avatar(
     backgroundColor: Color = DodamTheme.color.Background,
     shape: Shape = CircleShape,
     size: Dp = 40.dp,
+    rippleColor: Color = Color.Unspecified,
+    rippleEnable: Boolean = true,
+    bounded: Boolean = true,
+    onClick: (() -> Unit)? = null,
 ) {
     Box(
         modifier = modifier
             .size(size)
-            .surface(shape, backgroundColor),
+            .surface(shape, backgroundColor)
+            .dodamClickable(
+                onClick = onClick,
+                rippleColor = rippleColor,
+                rippleEnable = rippleEnable,
+                bounded = bounded
+            ),
         contentAlignment = Alignment.Center
     ) {
         IcUser(
             modifier = modifier.size(iconSize),
             tint = iconColor,
             contentDescription = null,
+        )
+    }
+}
+
+/**
+ * Dodam Avatar, can express remote image
+ *
+ * @param link remote image link
+ * @param modifier
+ * @param backgroundColor color for circle background
+ * @param shape shape of avatar, basic is circle
+ * @param size avatar circle size, basic is 40.dp
+ * @param placeHolderBaseColor loading place circle base color
+ * @param placeHolderHighlightColor loading place highlight bar color
+ * @param failureIconSize icon size when fail to load image
+ * @param failureIconColor icon color when fail to load image
+ * @param rippleColor
+ * @param rippleEnable
+ * @param bounded
+ * @param onClick when click avatar
+ */
+@Composable
+fun Avatar(
+    link: String,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = DodamTheme.color.Background,
+    shape: Shape = CircleShape,
+    size: Dp = 40.dp,
+    placeHolderBaseColor: Color = DodamTheme.color.White,
+    placeHolderHighlightColor: Color = DodamTheme.color.MainColor100,
+    failureIconSize: Dp = 20.dp,
+    failureIconColor: Color = DodamTheme.color.Gray400,
+    rippleColor: Color = Color.Unspecified,
+    rippleEnable: Boolean = true,
+    bounded: Boolean = true,
+    onClick: (() -> Unit)? = null,
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .surface(shape, backgroundColor)
+            .dodamClickable(
+                onClick = onClick,
+                rippleColor = rippleColor,
+                rippleEnable = rippleEnable,
+                bounded = bounded
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        GlideImage(
+            modifier = Modifier
+                .fillMaxSize(),
+            imageModel = { link },
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+            ),
+            component = rememberImageComponent {
+                +ShimmerPlugin(
+                    baseColor = placeHolderBaseColor,
+                    highlightColor = placeHolderHighlightColor
+                )
+            },
+            failure = {
+                IcUser(
+                    modifier = modifier
+                        .size(failureIconSize)
+                        .align(Alignment.Center),
+                    tint = failureIconColor,
+                    contentDescription = null,
+                )
+            }
         )
     }
 }
@@ -110,5 +261,7 @@ private fun AvatarPreview() {
         Spacer(modifier = Modifier.height(10.dp))
         Avatar(iconColor = DodamColor.FeatureColor.MyInfoColor)
         Avatar(iconColor = DodamColor.FeatureColor.MyInfoColor, size = 100.dp, iconSize = 50.dp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Avatar(link = "https://i.ytimg.com/vi/zbyet4HK4ko/maxresdefault.jpg", size = 100.dp)
     }
 }
