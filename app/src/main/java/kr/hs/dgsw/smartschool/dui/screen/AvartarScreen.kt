33810f53ee6075d27b713d7c60d1434b704ee671
DoHyeon <kdh200533@gmail.com>
2023-02-08 16:25:09 +0900

    ⚡️ :: Update AvatarScreen
    2023-02-08

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/AvartarScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/AvartarScreen.kt
@@ -1,25 +1,33 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kr.hs.dgsw.smartschool.components.component.appbar.DodamAppBar
-import kr.hs.dgsw.smartschool.components.theme.DodamColor
+import kr.hs.dgsw.smartschool.components.component.avatar.Avatar
+import kr.hs.dgsw.smartschool.components.theme.*
 
 @Composable
 @Preview(showBackground = true)
 fun AvatarPreview() {
-    InputScreen(navController = rememberNavController())
+    AvatarScreen(navController = rememberNavController())
 }
 
 @Composable
@@ -40,6 +48,137 @@ fun AvatarScreen(
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Spacer(Modifier.height(20.dp))
+            Title2(text = "Avatar")
+            Spacer(modifier = Modifier.height(20.dp))
+            Body2(text = "Basic Avatar")
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .width(300.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                BasicAvatar(label = "color = primary\nsize = 40.dp", iconColor = DodamColor.MainColor)
+                BasicAvatar(label = "color = secondary\nsize = 60.dp", iconColor = DodamColor.SecondaryColor,size = 60.dp)
+                BasicAvatar(label = "color = myInfo\nsize = 80.dp", iconColor = DodamColor.FeatureColor.MyInfoColor, size = 80.dp)
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Body2(text = "Name Avatar")
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .width(300.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                NameAvatar(
+                    label = "name = 홍길동\n" +
+                        "size = 40.dp",
+                    name = "홍길동"
+                )
+                NameAvatar(
+                    label = "name = 김김김\n" +
+                            "size = 60.dp",
+                    name = "김김김",
+                    size = 60.dp
+                )
+                NameAvatar(
+                    label = "name = John\n" +
+                        "size = 80.dp",
+                    name = "John",
+                    size = 80.dp
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Body2(text = "Image Avatar")
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .width(300.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                ImageAvatar(label = "img = dummy\nsize 40.dp", painter = painterResource(id = kr.hs.dgsw.smartschool.dui.R.drawable.img_dummy))
+                ImageAvatar(label = "img = dummy\nsize = 60.dp", painter = painterResource(id = kr.hs.dgsw.smartschool.dui.R.drawable.img_dummy), size = 60.dp)
+                ImageAvatar(label = "img = dummy\nsize = 80.dp", painter = painterResource(id = kr.hs.dgsw.smartschool.dui.R.drawable.img_dummy), size = 80.dp)
+            }
         }
+    }
+}
+
+@Composable
+fun NameAvatar(
+    label : String,
+    name: String,
+    modifier: Modifier = Modifier,
+    nameStyle: TextStyle = DodamTheme.typography.title2,
+    nameColor: Color = DodamTheme.color.Gray400,
+    backgroundColor: Color = DodamTheme.color.Background,
+    shape: Shape = CircleShape,
+    size: Dp = 40.dp,
+    ){
+    Column(
+        modifier = Modifier
+            .width(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Avatar(
+            name = name,
+            modifier = modifier,
+            nameStyle = nameStyle,
+            nameColor = nameColor,
+            backgroundColor = backgroundColor,
+            shape = shape,
+            size = size
+        )
+        Label2(text = label, textAlign = TextAlign.Center)
+    }
+}
+@Composable
+fun ImageAvatar(
+    label : String,
+    painter: Painter,
+    modifier: Modifier = Modifier,
+    shape: Shape = CircleShape,
+    size: Dp = 40.dp,
+){
+    Column(
+        modifier = Modifier
+            .width(100.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Avatar(
+            painter = painter,
+            modifier = modifier,
+            shape = shape,
+            size = size
+        )
+        Label2(text = label, textAlign = TextAlign.Center)
+    }
+}
+@Composable
+fun BasicAvatar(
+    label : String,
+    modifier: Modifier = Modifier,
+    iconSize: Dp = 20.dp,
+    iconColor: Color = DodamTheme.color.Gray400,
+    backgroundColor: Color = DodamTheme.color.Background,
+    shape: Shape = CircleShape,
+    size: Dp = 40.dp,
+    ){
+    Column(
+        modifier = Modifier
+            .width(100.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Avatar(
+            modifier = modifier,
+            iconSize = iconSize,
+            iconColor = iconColor,
+            backgroundColor = backgroundColor,
+            shape = shape,
+            size = size
+        )
+        Label2(text = label, textAlign = TextAlign.Center)
     }
 }
