6ee70a2c1b7212112d2de2299d5269b8def87602
DoHyeon <kdh200533@gmail.com>
2023-02-12 00:40:30 +0900

    ⚡️ :: Add Banners
    2023-02-12

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BannerScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BannerScreen.kt
@@ -1,9 +1,8 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -14,7 +13,10 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kr.hs.dgsw.smartschool.components.component.set.appbar.DodamAppBar
+import kr.hs.dgsw.smartschool.components.component.set.banner.Banner
 import kr.hs.dgsw.smartschool.components.theme.DodamColor
+import kr.hs.dgsw.smartschool.components.theme.Title2
+import kr.hs.dgsw.smartschool.components.utlis.DodamDimen
 import kr.hs.dgsw.smartschool.dui.DataSet
 
 @Composable
@@ -41,6 +43,37 @@ fun BannerScreen(
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(DodamDimen.ScreenSidePadding)
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                Title2(text = "Banner")
+                Spacer(modifier = Modifier.height(10.dp))
+                Banner(
+                    imageUrls = listOf(
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/c70c7b96-dd67-4467-a49c-2d0baa459624TEAM%20B1ND%20Banner.png",
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/be5fa717-a286-42e8-bcb3-40c57bfd61cbB1ND%20%E1%84%89%E1%85%AE%E1%84%89%E1%85%B5%20%E1%84%8E%E1%85%A2%E1%84%8B%E1%85%AD%E1%86%BCbanner%20(1).png",
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/be5fa717-a286-42e8-bcb3-40c57bfd61cbB1ND%20%E1%84%89%E1%85%AE%E1%84%89%E1%85%B5%20%E1%84%8E%E1%85%A2%E1%84%8B%E1%85%AD%E1%86%BCbanner%20(1).png",
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Banner(
+                    imageUrls = listOf(
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/c70c7b96-dd67-4467-a49c-2d0baa459624TEAM%20B1ND%20Banner.png",
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Banner(
+                    imageUrls = listOf(
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/c70c7b96-dd67-4467-a49c-2d0baa459624TEAM%20B1ND%20Banner.png",
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/be5fa717-a286-42e8-bcb3-40c57bfd61cbB1ND%20%E1%84%89%E1%85%AE%E1%84%89%E1%85%B5%20%E1%84%8E%E1%85%A2%E1%84%8B%E1%85%AD%E1%86%BCbanner%20(1).png",
+                        "https://dodam.kr.object.ncloudstorage.com/dodam/be5fa717-a286-42e8-bcb3-40c57bfd61cbB1ND%20%E1%84%89%E1%85%AE%E1%84%89%E1%85%B5%20%E1%84%8E%E1%85%A2%E1%84%8B%E1%85%AD%E1%86%BCbanner%20(1).png",
+                    ),
+                    showIndicator = false
+                )
+            }
         }
     }
 }
