a49062df1ca69716e8f200d22b61ec6228268f34
DoHyeon <kdh200533@gmail.com>
2023-02-12 00:14:09 +0900

    ⚡️ :: Add BottomSheet Dialog
    2023-02-12

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BottomSheetScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BottomSheetScreen.kt
@@ -1,9 +1,11 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +15,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kr.hs.dgsw.smartschool.components.component.basic.button.DodamLargeRoundedButton
 import kr.hs.dgsw.smartschool.components.component.organization.bottomsheet.BottomSheet
+import kr.hs.dgsw.smartschool.components.component.organization.bottomsheet.BottomSheetDialog
 import kr.hs.dgsw.smartschool.components.component.organization.card.DodamItemCard
 import kr.hs.dgsw.smartschool.components.component.set.appbar.DodamAppBar
 import kr.hs.dgsw.smartschool.components.theme.*
@@ -23,7 +26,7 @@ import kr.hs.dgsw.smartschool.dui.DataSet
 fun BottomSheetPreview() {
     BottomSheetScreen(navController = rememberNavController())
 }
-
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BottomSheetScreen(
     navController: NavController
@@ -33,7 +36,9 @@ fun BottomSheetScreen(
             .fillMaxSize()
             .background(DodamColor.Background)
     ) {
-        DodamAppBar(title = DataSet.Text.TITLE_BOTTOM_SHEET, onStartIconClick = { navController.popBackStack() })
+        DodamAppBar(
+            title = DataSet.Text.TITLE_BOTTOM_SHEET,
+            onStartIconClick = { navController.popBackStack() })
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -42,6 +47,9 @@ fun BottomSheetScreen(
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Spacer(modifier = Modifier.height(20.dp))
+            Title2(text = "BottomSheet")
+            Spacer(modifier = Modifier.height(10.dp))
             Column(
                 modifier = Modifier
                     .height(400.dp)
@@ -49,23 +57,48 @@ fun BottomSheetScreen(
                     .background(DodamColor.White)
                     .padding(10.dp)
             ) {
-                Title2(text = "외부 박스")
                 BottomSheet(
                     sheetContent = {
-                        Column {
-                            DodamItemCard(title = "Hello", subTitle = "test")
-                            DodamItemCard(title = "Hello", subTitle = "test")
-                            DodamItemCard(title = "Hello", subTitle = "test")
-                        }
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                    },
+                    content = {
+                        Display1(text = "Content")
                     }
-                ) {
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .background(DodamColor.White)
-                    ) {
-                        Title2(text = "내부 박스")
-                    }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Title2(text = "BottomSheet Dialog")
+            Spacer(modifier = Modifier.height(10.dp))
+            Column(
+                modifier = Modifier
+                    .height(400.dp)
+                    .fillMaxWidth()
+                    .background(DodamColor.White)
+                    .padding(10.dp)
+            ) {
+                BottomSheetDialog(
+                    sheetTopContent = {
+                        Spacer(modifier = Modifier.height(18.dp))
+                        Label1(
+                            text = "Title",
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
+                    },
+                    sheetBottomContent = {
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                        DodamItemCard(title = "TestTitle", subTitle = "title")
+                    },
+                ) { sheetState ->
+                    Display1(text = "Content", onClick = {
+
+                    })
                 }
             }
         }
