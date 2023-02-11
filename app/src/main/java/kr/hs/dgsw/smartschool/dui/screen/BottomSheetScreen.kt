1a85f899bdefd440fc0ea3754812aa2edfb9b455
DoHyeon <kdh200533@gmail.com>
2023-02-11 22:59:08 +0900

    🎨 :: Modify BottomSheet PreviewDesign
    2023-02-11

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BottomSheetScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/BottomSheetScreen.kt
@@ -11,11 +11,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import kr.hs.dgsw.smartschool.components.component.basic.button.DodamLargeRoundedButton
 import kr.hs.dgsw.smartschool.components.component.organization.bottomsheet.BottomSheet
+import kr.hs.dgsw.smartschool.components.component.organization.card.DodamItemCard
 import kr.hs.dgsw.smartschool.components.component.set.appbar.DodamAppBar
-import kr.hs.dgsw.smartschool.components.theme.DodamColor
-import kr.hs.dgsw.smartschool.components.theme.IcHome
-import kr.hs.dgsw.smartschool.components.theme.Title2
+import kr.hs.dgsw.smartschool.components.theme.*
 import kr.hs.dgsw.smartschool.dui.DataSet
 
 @Composable
@@ -31,24 +31,42 @@ fun BottomSheetScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(DodamColor.White)
+            .background(DodamColor.Background)
     ) {
         DodamAppBar(title = DataSet.Text.TITLE_BOTTOM_SHEET, onStartIconClick = { navController.popBackStack() })
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(20.dp)
-                .background(DodamColor.White)
+                .background(DodamColor.Background)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            BottomSheet(
-                sheetContent = {
-                    IcHome(contentDescription = null)
-                }
+            Column(
+                modifier = Modifier
+                    .height(400.dp)
+                    .fillMaxWidth()
+                    .background(DodamColor.White)
+                    .padding(10.dp)
             ) {
-                Spacer(modifier = Modifier.height(20.dp))
-                Title2(text = DataSet.Text.TITLE_BOTTOM_SHEET)
+                Title2(text = "외부 박스")
+                BottomSheet(
+                    sheetContent = {
+                        Column {
+                            DodamItemCard(title = "Hello", subTitle = "test")
+                            DodamItemCard(title = "Hello", subTitle = "test")
+                            DodamItemCard(title = "Hello", subTitle = "test")
+                        }
+                    }
+                ) {
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .background(DodamColor.White)
+                    ) {
+                        Title2(text = "내부 박스")
+                    }
+                }
             }
         }
     }
