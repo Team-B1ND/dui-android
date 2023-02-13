33d1bd122059da3aa76637ce332553ec20480446
DoHyeon <kdh200533@gmail.com>
2023-02-13 17:39:28 +0900

    🎨 ::  ktLint
    2023-02-13

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/CalenderScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/CalenderScreen.kt
@@ -1,12 +1,21 @@
 package kr.hs.dgsw.smartschool.dui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,7 +63,7 @@ fun CalenderScreen(
         ) {
             var selectedDay by remember { mutableStateOf(LocalDate.now()) }
             var selectedSchedules by remember { mutableStateOf(emptyList<DaySchedule>()) }
-            DodamCalendar(schedules = sampleSchedules){date, daySchedules ->  
+            DodamCalendar(schedules = sampleSchedules) { date, daySchedules ->
                 selectedDay = date
                 selectedSchedules = daySchedules
             }
