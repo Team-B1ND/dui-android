e9fe46b5a3ec9333e17ec98a396948f46f443fa6
DoHyeon <kdh200533@gmail.com>
2023-02-16 21:21:41 +0900

    🎨 ::  ktLint
    2023-02-16

--- app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/InputScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dui/screen/InputScreen.kt
@@ -9,7 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,7 +22,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kr.hs.dgsw.smartschool.components.component.basic.input.DodamInput
-import kr.hs.dgsw.smartschool.components.component.basic.input.DodamSelect
 import kr.hs.dgsw.smartschool.components.component.basic.input.area.DodamInputArea
 import kr.hs.dgsw.smartschool.components.component.set.appbar.DodamAppBar
 import kr.hs.dgsw.smartschool.components.theme.DodamColor
@@ -106,8 +109,7 @@ fun SampleInputArea(
 }
 
 @Composable
-fun SampleInputArea(
-){
+fun SampleInputArea() {
     Spacer(modifier = Modifier.height(20.dp))
     Title2(text = "Input Area")
     Spacer(modifier = Modifier.height(20.dp))
